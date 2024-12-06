e7a899160cb8b79e7b76c250dde6d188c4bcfda9
Suyash Bhawsar <suyashbhawsar@hotmail.com>
2024-12-06 18:50:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ git clone git@gitlab.com:seaside1/crypto-hunters-ar-game/crypto-hunters-mobile-a
 
 cd crypto-hunters-ar-game
 git checkout refactor_integration_IOS
+mkdir -p Assets/Editor
 cd ..
 cd crypto-hunters-mobile-app
 git checkout current
@@ -11,4 +12,5 @@ yarn install
 mkdir -p unity/builds/ios
 cd ..
 
+wget -O crypto-hunters-ar-game/Assets/Editor/AutoBuild.cs https://raw.githubusercontent.com/suyashbhawsar/unity-xcode-gitlab-cicd/refs/heads/main/AutoBuild.cs
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -quit -projectPath crypto-hunters-ar-game -executeMethod BuildTools.AutoBuild.BuildProject
